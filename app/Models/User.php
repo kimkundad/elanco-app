c92d6a92a3b70d05c60d0df6ece833bfef9870ab
KIM_KUNDAD <kim.kundad@gmail.com>
2024-12-26 10:06:44 +0700

    kim

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,6 +70,7 @@ class User extends Authenticatable implements JWTSubject
         'password', 'remember_token',
     ];
 
+
     /**
      * The attributes that should be cast to native types.
      *
