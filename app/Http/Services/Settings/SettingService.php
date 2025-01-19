ab276b699c5fac473d5564ce9e1162d4f447c5eb
ph.kongkiat <ph.kongkiat@gmail.com>
2025-01-19 18:06:26 +0700

    - include featured courses in settings GET API

--- app/Http/Services/Settings/SettingService.php
+++ app/Http/Services/Settings/SettingService.php
@@ -6,18 +6,21 @@ class SettingService
 {
     private PageBannerService $pageBannerService;
     private HomeBannerService $homeBannerService;
+    private FeaturedCourseService $featuredCourseService;
 
-    public function __construct(PageBannerService $pageBannerService, HomeBannerService $homeBannerService)
+    public function __construct(PageBannerService $pageBannerService, HomeBannerService $homeBannerService, FeaturedCourseService $featuredCourseService)
     {
         $this->pageBannerService = $pageBannerService;
         $this->homeBannerService = $homeBannerService;
+        $this->featuredCourseService = $featuredCourseService;
     }
 
     public function findAll()
     {
         return [
             'pageBanners' => $this->pageBannerService->findAll(),
-            'homeBanners' => $this->homeBannerService->findAll()
+            'homeBanners' => $this->homeBannerService->findAll(),
+            'featuredCourses' => $this->featuredCourseService->findAll(),
         ];
     }
 }
